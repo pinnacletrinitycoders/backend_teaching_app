2f84b6a053073fa637576708061e8d47cb0aa7d9
pinnacletrinitycoders <pinnacletrinitycoders@gmail.com>
2024-02-01 22:28:58 +0530

    startups

---
+++ configs/dbConfig.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+module.exports = {
+    DB_NAME: process.env.DB_NAME,
+    DB_URL: process.env.DB_URL
+}
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose');
+const app = express()
+
+//configs
+const { PORT } = require('./configs/serverConfig')
+const { DB_URL, DB_NAME } = require('./configs/dbConfig')
+
+// connection to database
+mongoose.connect(DB_URL);
+const db = mongoose.connection
+
+db.on('error', (err) => {
+    console.log(`❌This is error : ${err}`);
+})
+db.once('open', () => {
+    console.log(`✅Connected with database Successfully : ${DB_NAME}`);
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`SERVER IS RUNNING ON PORT ${PORT}`)
+})
